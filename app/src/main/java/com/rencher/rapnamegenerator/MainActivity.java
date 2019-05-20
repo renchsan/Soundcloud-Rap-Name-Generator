fb056df517fb0146237d31910bff4414a7bb40f2
mrench <mattrencher@gmail.com>
2019-05-19 23:56:51 -0400

    initial commit

---
+++ app/src/main/java/com/rencher/rapnamegenerator/MainActivity.java
@@ -0,0 +1,35 @@
+package com.rencher.rapnamegenerator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView namesView;
+    Button btn1;
+    String newNames = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.button1);
+        namesView = findViewById(R.id.namesView);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newNames = (namegenerator.generateName(12));
+                namesView.setText(newNames);
+            }
+        });
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/rencher/rapnamegenerator/namegenerator.java
@@ -0,0 +1,37 @@
+package com.rencher.rapnamegenerator;
+
+import java.util.Random;
+
+public class namegenerator {
+    private static String[] Beginning = { "Lil " , "lil ", "Gang ", "White ", "Old ",
+            "Yung ", "XXX", "Smoke" , "Ugly ", "Nasty ", "Big ", "21", "Lean ", "Polo", "Rich"};
+    private static String[] Middle = { "Air", "Tril", "Gang", "Ghost", "Sip", "Clo",
+            "Red", "Mask", "Pump", "Gat", "Murder", "Lock", "Cree", "Mur", "Zed", "Slime",
+            "Kid", "Boy", "Skee", "Purp", "Daddy", " Rich", "Lean", "Famous"};
+    private static String[] End = { "D", "baby", "trigger", "juice",
+            "tron", "med", "jay", "sticky", "mane", "mur", "weed", "candy", " Choppa"," WRLD",
+            "homie"," Jesus", "cloud"};
+
+    private static Random rand = new Random();
+
+
+    public static String generateName(int totalNames) {
+        String names = "";
+        String newName;
+        String prevName = "";
+
+        for (int i = 0; i < totalNames; i++){
+            newName = Beginning[rand.nextInt(Beginning.length)]+
+                    Middle[rand.nextInt(Middle.length)]+
+                    End[rand.nextInt(End.length)];
+
+            if(!(prevName.equals(newName))) {
+                names += newName;
+                names += "\n";
+            }
+            prevName = newName;
+        }
+
+        return names;
+    }
+}
